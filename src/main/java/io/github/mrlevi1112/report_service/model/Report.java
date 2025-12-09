d6a1c526b9378120efd918d464fd3fe0b8a9e18e
ido levi <ido1112levi@gmail.com>
2025-12-09 17:03:34 +0200

    Initialize report-service project

---
+++ src/main/java/io/github/mrlevi1112/report_service/model/Report.java
@@ -0,0 +1,33 @@
+package main.java.io.github.mrlevi1112.report_service.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection = "reports") 
+public class Report {
+
+    @Id
+    private String id; 
+
+    private String username; 
+    
+    private LocalDateTime eventDate;
+    private String description;
+    
+    private List<String> imageUrls;
+    
+    private String status;
+    
+    private Double estimatedCost;
+}
