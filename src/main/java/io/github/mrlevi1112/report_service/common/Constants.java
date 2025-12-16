ac679ec40ec4a1595554d65c875eca8becd29978
ido levi <ido1112levi@gmail.com>
2025-12-16 15:23:56 +0200

    Adds private constructor to Constants class
    Adds a private constructor to the Constants class
    to prevent instantiation, as it is intended to
    hold only static constants.

--- src/main/java/io/github/mrlevi1112/report_service/common/Constants.java
+++ src/main/java/io/github/mrlevi1112/report_service/common/Constants.java
@@ -1,6 +1,8 @@
 package io.github.mrlevi1112.report_service.common;
 
 public class Constants {
+    private Constants() {}
+    
     public static final String STATUS_RECEIVED = "RECEIVED";
     public static final String BEARER_PREFIX = "Bearer ";
 }
